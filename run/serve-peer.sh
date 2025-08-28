99844d1730fee617caf90e23e35df4a22fba06a6
David Vargas <dav793@gmail.com>
2025-08-27 20:24:37 -0600

    Fix unix shell scripts

--- run/serve-peer.sh
+++ run/serve-peer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd "$(dirname "$0")"    # use script's location as working directory
 
-set -a && . ../.env && node --enable-source-maps dist/peer.js
+set -a && . ../.env && node --enable-source-maps ../dist/peer.js
--- run/serve-signal.sh
+++ run/serve-signal.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd "$(dirname "$0")"    # use script's location as working directory
 
-set -a && . ../.env && node --enable-source-maps dist/signaling-server.js
+set -a && . ../.env && node --enable-source-maps ../dist/signaling-server.js
