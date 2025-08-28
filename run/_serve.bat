a057eb45d83809557979759e59bb02fcb9dbb699
David Vargas <dav793@gmail.com>
2025-08-28 02:43:58 -0600

    Fix scripts: bat, bash

--- run/_serve.bat
+++ run/_serve.bat
@@ -1,6 +1,6 @@
 @echo off
 
-rem Read and set variables from .env
+rem read and set variables from .env
 for /f "tokens=1* delims==" %%a in (.env) do (
     set "%%a=%%b"
 )
--- run/_serve.sh
+++ run/_serve.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"    # use script's location as working directory
 
-export $(cat ../.env | grep -v '^#' | xargs)
-node --enable-source-maps ../dist/${SCRIPT_PATH}
+export $(cat ../.env | grep -v '^#' | xargs)    # read and set variables from .env
 
-#set -a && . ../.env && node --enable-source-maps ../dist/${SCRIPT_PATH}
+node --enable-source-maps ../dist/${SCRIPT_PATH}
